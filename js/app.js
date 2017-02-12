940bd29b64f2a557439bf36451cbd0d31899c357
Aymeric <aymeric.gioia@hotmail.fr>
2017-02-12 21:44:00 +0100

    ajout fonction suppr

--- js/app.js
+++ js/app.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
-	alert("Ouai c'est pas faux!");
+	alert("Bienvenue")
 	console.log("Bienvenue utilisateur");
-	$("#todo-list").append("<li>" + "AVANT DE COMMENCER ANALYSER LE CODE" + "</li>")
-
-	$(function ($){
-		"use strict";
-	});
-
-	var ENTER_KEY = 13;
-	var ESCAPE_KEY = 27;
-
-
+	$("#todo-list").append("<li>" + "Ma todo-list" + "</li>")
 
 	$('.ajout').click(function(){
 		var tache=$("#new-todo").val();
 		$("#todo-list").append("<li><input type=checkbox>" + tache + "</li>")
 	});
-
-
-
+	$(".supp").on("click", function() {
+		$("#main input:checked").parent().remove();
+	});
 
 });
